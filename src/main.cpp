8c616c72f833830445ea335c8397ba266304287b
J4m3s00 <james.lol2000@gmail.com>
2021-04-06 19:15:25 +0200

    Use docking for imgui

--- src/main.cpp
+++ src/main.cpp
@@ -57,6 +57,9 @@ int main()
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
     ImGuiIO& io = ImGui::GetIO(); (void)io;
+    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;           // Enable Docking
+    io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;         // Enable Multi-Viewport / Platform Windows
+    
     //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
     //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
 
@@ -105,14 +108,55 @@ int main()
 
         ImGui::NewFrame();
 
+        static bool p_open;
+
+        static bool opt_fullscreen_persistant = true;
+        static ImGuiDockNodeFlags opt_flags = ImGuiDockNodeFlags_None;
+        bool opt_fullscreen = opt_fullscreen_persistant;
+
+        ImGuiWindowFlags window_flags = ImGuiWindowFlags_MenuBar;
+        if (opt_fullscreen)
+        {
+            ImGuiViewport* viewport = ImGui::GetMainViewport();
+            ImGui::SetNextWindowPos(viewport->Pos);
+            ImGui::SetNextWindowSize(viewport->Size);
+            ImGui::SetNextWindowViewport(viewport->ID);
+            ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
+            ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
+            window_flags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
+            window_flags |= ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
+        }
+
+
+        ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+        ImGui::Begin("DockSpace Demo", &p_open, window_flags);
+        ImGui::PopStyleVar();
+
+
+        if (opt_fullscreen)
+            ImGui::PopStyleVar(2);
+
+        // Dockspace		         
+        ImGuiIO& io = ImGui::GetIO();		        
+        if (io.ConfigFlags & ImGuiConfigFlags_DockingEnable)
+        {		
+            ImGuiID dockspace_id = ImGui::GetID("MyDockspace");		
+            ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), opt_flags);		
+        }
+
         ImGui::Begin("Hello World");
         ImGui::Image((ImTextureID)(unsigned long)frameBuffer->GetColorAttachment(), {500, 500});
+        ImGui::End();
+
+
+        
         ImGui::End();
 
         ImGui::Render();
 
 
         G_RENDERCMD2(window, frameBuffer, {
+            ImGuiIO& io = ImGui::GetIO();
             // Start the Dear ImGui frame
             ImGui_ImplOpenGL3_NewFrame();
             
@@ -123,6 +167,17 @@ int main()
             glClearColor(0, 0, 0, 1);
             glClear(GL_COLOR_BUFFER_BIT);
             ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+            // Update and Render additional Platform Windows
+            // (Platform functions may change the current OpenGL context, so we save/restore it to make it easier to paste this code elsewhere.
+            //  For this specific demo app we could also call glfwMakeContextCurrent(window) directly)
+            if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+            {
+                GLFWwindow* backup_current_context = glfwGetCurrentContext();
+                ImGui::UpdatePlatformWindows();
+                ImGui::RenderPlatformWindowsDefault();
+                glfwMakeContextCurrent(backup_current_context);
+            }
         })
 
 
