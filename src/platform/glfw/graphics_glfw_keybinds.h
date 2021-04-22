416b541c95d56d32a0af65badc146d4e43c6366f
J4m3s00 <james.lol2000@gmail.com>
2021-04-22 14:05:33 +0200

    add key binds

---
+++ src/platform/glfw/graphics_glfw_keybinds.h
@@ -0,0 +1,126 @@
+#pragma once
+
+#define USK_KEY_SPACE              32
+#define USK_KEY_APOSTROPHE         39  /* ' */
+#define USK_KEY_COMMA              44  /* , */
+#define USK_KEY_MINUS              45  /* - */
+#define USK_KEY_PERIOD             46  /* . */
+#define USK_KEY_SLASH              47  /* / */
+#define USK_KEY_0                  48
+#define USK_KEY_1                  49
+#define USK_KEY_2                  50
+#define USK_KEY_3                  51
+#define USK_KEY_4                  52
+#define USK_KEY_5                  53
+#define USK_KEY_6                  54
+#define USK_KEY_7                  55
+#define USK_KEY_8                  56
+#define USK_KEY_9                  57
+#define USK_KEY_SEMICOLON          59  /* ; */
+#define USK_KEY_EQUAL              61  /* = */
+#define USK_KEY_A                  65
+#define USK_KEY_B                  66
+#define USK_KEY_C                  67
+#define USK_KEY_D                  68
+#define USK_KEY_E                  69
+#define USK_KEY_F                  70
+#define USK_KEY_G                  71
+#define USK_KEY_H                  72
+#define USK_KEY_I                  73
+#define USK_KEY_J                  74
+#define USK_KEY_K                  75
+#define USK_KEY_L                  76
+#define USK_KEY_M                  77
+#define USK_KEY_N                  78
+#define USK_KEY_O                  79
+#define USK_KEY_P                  80
+#define USK_KEY_Q                  81
+#define USK_KEY_R                  82
+#define USK_KEY_S                  83
+#define USK_KEY_T                  84
+#define USK_KEY_U                  85
+#define USK_KEY_V                  86
+#define USK_KEY_W                  87
+#define USK_KEY_X                  88
+#define USK_KEY_Y                  89
+#define USK_KEY_Z                  90
+#define USK_KEY_LEFT_BRACKET       91  /* [ */
+#define USK_KEY_BACKSLASH          92  /* \ */
+#define USK_KEY_RIGHT_BRACKET      93  /* ] */
+#define USK_KEY_GRAVE_ACCENT       96  /* ` */
+#define USK_KEY_WORLD_1            161 /* non-US #1 */
+#define USK_KEY_WORLD_2            162 /* non-US #2 */
+
+/* Function keys */
+#define USK_KEY_ESCAPE             256
+#define USK_KEY_ENTER              257
+#define USK_KEY_TAB                258
+#define USK_KEY_BACKSPACE          259
+#define USK_KEY_INSERT             260
+#define USK_KEY_DELETE             261
+#define USK_KEY_RIGHT              262
+#define USK_KEY_LEFT               263
+#define USK_KEY_DOWN               264
+#define USK_KEY_UP                 265
+#define USK_KEY_PAGE_UP            266
+#define USK_KEY_PAGE_DOWN          267
+#define USK_KEY_HOME               268
+#define USK_KEY_END                269
+#define USK_KEY_CAPS_LOCK          280
+#define USK_KEY_SCROLL_LOCK        281
+#define USK_KEY_NUM_LOCK           282
+#define USK_KEY_PRINT_SCREEN       283
+#define USK_KEY_PAUSE              284
+#define USK_KEY_F1                 290
+#define USK_KEY_F2                 291
+#define USK_KEY_F3                 292
+#define USK_KEY_F4                 293
+#define USK_KEY_F5                 294
+#define USK_KEY_F6                 295
+#define USK_KEY_F7                 296
+#define USK_KEY_F8                 297
+#define USK_KEY_F9                 298
+#define USK_KEY_F10                299
+#define USK_KEY_F11                300
+#define USK_KEY_F12                301
+#define USK_KEY_F13                302
+#define USK_KEY_F14                303
+#define USK_KEY_F15                304
+#define USK_KEY_F16                305
+#define USK_KEY_F17                306
+#define USK_KEY_F18                307
+#define USK_KEY_F19                308
+#define USK_KEY_F20                309
+#define USK_KEY_F21                310
+#define USK_KEY_F22                311
+#define USK_KEY_F23                312
+#define USK_KEY_F24                313
+#define USK_KEY_F25                314
+#define USK_KEY_KP_0               320
+#define USK_KEY_KP_1               321
+#define USK_KEY_KP_2               322
+#define USK_KEY_KP_3               323
+#define USK_KEY_KP_4               324
+#define USK_KEY_KP_5               325
+#define USK_KEY_KP_6               326
+#define USK_KEY_KP_7               327
+#define USK_KEY_KP_8               328
+#define USK_KEY_KP_9               329
+#define USK_KEY_KP_DECIMAL         330
+#define USK_KEY_KP_DIVIDE          331
+#define USK_KEY_KP_MULTIPLY        332
+#define USK_KEY_KP_SUBTRACT        333
+#define USK_KEY_KP_ADD             334
+#define USK_KEY_KP_ENTER           335
+#define USK_KEY_KP_EQUAL           336
+#define USK_KEY_LEFT_SHIFT         340
+#define USK_KEY_LEFT_CONTROL       341
+#define USK_KEY_LEFT_ALT           342
+#define USK_KEY_LEFT_SUPER         343
+#define USK_KEY_RIGHT_SHIFT        344
+#define USK_KEY_RIGHT_CONTROL      345
+#define USK_KEY_RIGHT_ALT          346
+#define USK_KEY_RIGHT_SUPER        347
+#define USK_KEY_MENU               348
+
+#define USK_KEY_LAST               USK_KEY_MENU
