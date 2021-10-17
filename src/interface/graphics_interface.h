052060778108dccc475b5faf1f15dadf4194576c
J4m3s00 <james.lol2000@gmail.com>
2021-10-17 18:35:37 +0200

    fix build

--- src/interface/graphics_interface.h
+++ src/interface/graphics_interface.h
@@ -5,8 +5,8 @@
 #include <vector>
 #include <functional>
 
-#include "glm/glm.hpp"
 #define GLM_ENABLE_EXPERIMENTAL
+#include "glm/glm.hpp"
 #include "glm/gtc/type_ptr.hpp"
 #include "glm/gtx/matrix_decompose.hpp"
 #include "glm/gtc/matrix_transform.hpp"
